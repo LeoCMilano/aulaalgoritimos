a88e74f314f9e8a2fb1955fe7e3138b2cac9ff21
Leo Milano <lmilano@outlook.com>
2024-01-03 15:27:02 -0300

    Arrays da aula

--- selectionSort.js
+++ selectionSort.js
@@ -1,18 +1,13 @@
-const livros = require('./listaLivros.json');
-const menorValor = require('./menorValor');
+const livros = require ("./listaLivros.json")
 
-              // 1
-for (let atual = 0; atual < livros.length - 1; atual++) {
-  let menor = menorValor(livros, atual)
-
-  let livroAtual = livros[atual];
-  console.log('posição atual', atual)
-  console.log('livro atual', livros[atual])
-  let livroMenorPreco = livros[menor];
-  console.log('livro menor preço', livros[menor])
-
-  livros[atual] = livroMenorPreco
-  livros[menor] = livroAtual
-}
-
-console.log(livros)
+livros.forEach((_, indice) => {
+    let menor = menorValor(livros, indice)
+   
+    let livroAtual = livros[indice];
+    let livroMenorPreco = livros[menor];
+   
+    livros[indice] = livroMenorPreco
+    livros[menor] = livroAtual 
+   })
+   
+   console.log(livros)
